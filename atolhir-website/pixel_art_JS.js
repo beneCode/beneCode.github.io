483bca65c2e3e9a27a51a6f2c37cb23ee244db0b
beneCode <75844072+beneCode@users.noreply.github.com>
2020-12-11 14:29:06 +0100

    Add files via upload

---
+++ atolhir-website/pixel_art_JS.js
@@ -0,0 +1,24 @@
+//background
+var canvas = document.getElementById("background"),
+    context = canvas.getContext("2d"),
+    img = new Image();
+
+img.src = 'website-background.png';
+
+img.onload = function()
+{
+    // create pattern
+    var ptrn = context.createPattern(img, 'repeat'); // Create a pattern with this image, and set it to "repeat".
+    context.fillStyle = ptrn;
+    context.fillRect(0, 0, canvas.width, canvas.height); // context.fillRect(x, y, width, height);
+}
+
+//title
+var ctx = document.getElementById('title').getContext('2d');
+// load image
+var image = new Image();
+image.onload = function () {
+    // draw the image into the canvas
+    ctx.drawImage(image, 0, 0);
+}
+image.src = 'Atolhir-mein-title-text.png';
